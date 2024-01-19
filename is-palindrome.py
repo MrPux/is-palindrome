7d720456111c60801942648ffed5b2f735dfb3cb
MrPuxe <anthondeleonsketch@gmail.com>
2024-01-19 17:50:17 -0500

    Code with a finished function and comments

--- is-palindrome.py
+++ is-palindrome.py
@@ -1,1 +1,6 @@
 # is_palindrome("radar")  # Should return True
+def is_palindrome(string):
+    palindrome = ''.join(i for i in list(string)[::-1])
+    return False if palindrome != string else True
+
+is_palindrome("radar") #It returns "True"
